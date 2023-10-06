c2db5761e298bb9548356079de47cc8e1cdb85a9
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-10-06 22:12:23 +0200

    sort -f translations and loop updating script
    add missing package installer string

---
+++ compare_translations.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#Compares 
+for file in assets/translations/*
+do
+  if [ -f "$file" ]; then
+	sed 's/=.*//' $file > temp
+	grep -f temp translate.txt > patterns
+	grep -f patterns $file > LANG_diff
+	grep -vf patterns translate.txt > temp
+	cp LANG_diff temp2
+	cat temp > LANG_missing
+	cat temp >> temp2
+	sort -f temp2 > $file
+	rm temp temp2 patterns
+	echo -e "\n\n List of missing translations:\n\n"
+	cat LANG_missing
+	echo -e "\n\n List of valid translations:\n\n"
+	cat LANG_diff
+	rm LANG_missing LANG_diff
+  fi
+done
