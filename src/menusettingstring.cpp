0c2142927114bff9d5006fab91b5c6917da1f916
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-12-02 21:46:57 +0100

    read String value in settingsDialog (TTS)

--- src/menusettingstring.cpp
+++ src/menusettingstring.cpp
@@ -36,6 +36,7 @@ MenuSettingStringBase(gmenu2x, title, description, value), dialogTitle(dialogTit
 }
 
 void MenuSettingString::edit() {
+	gmenu2x->allyTTS(value().c_str(), MEDIUM_GAP_TTS, MEDIUM_SPEED_TTS);
 	InputDialog id(gmenu2x, /*gmenu2x->ts,*/ description, value(), dialogTitle, dialogIcon);
 	if (id.exec()) setValue(id.getInput());
 }
