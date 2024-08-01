fac251a407dbf238a1ce24c4e95548c84a7a7a83
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-08-01 22:40:22 +0200

    ipkInstall: don't remount rootfs for opkg install/remove
    it's already done by custom opkg from BR2 (see https://github.com/MiyooCFW/buildroot/commit/4836a07b732bdfec8e7d73352723635a823d89da)

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -2101,10 +2101,8 @@ void GMenu2X::deleteLink() {
 				unlink(package.c_str());
 				menu->deleteSelectedLink();
 			} else if (package_type == 2) {
-				system("mount -o remount,rw /");
 				TerminalDialog td(this, tr["Uninstall package"], "opkg remove " + package, "skin:icons/configure.png");
 				td.exec("opkg remove " + package);
-				system("mount -o remount,ro /");
 			}
 			initMenu();
 			break;
@@ -2191,13 +2189,11 @@ string GMenu2X::ipkName(string cmd) {
 }
 
 void GMenu2X::ipkInstall(string path) {
-	system("mount -o remount,rw /");
 	string cmd = "opkg install --force-reinstall --force-overwrite ";
 	input.update(false);
 	if (input[MANUAL]) cmd += "--force-downgrade ";
 	TerminalDialog td(this, tr["Package installer"], "opkg install " + base_name(path), "skin:icons/configure.png");
 	td.exec(cmd + cmdclean(path));
-	system("mount -o remount,ro /");
 	//system("if [ -d sections/systems ]; then mkdir -p sections/emulators.systems; cp -r sections/systems/* sections/emulators.systems/; rm -rf sections/systems; fi; sync;");
 	initMenu();
 }
