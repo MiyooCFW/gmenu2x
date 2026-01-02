041911ecfe4ccd4cfcc6e729a85541eab0fa9883
Apaczer <94932128+Apaczer@users.noreply.github.com>
2026-01-02 21:36:45 +0100

    fix Previews also in WallpaperDialog

--- src/wallpaperdialog.cpp
+++ src/wallpaperdialog.cpp
@@ -80,10 +80,10 @@ bool WallpaperDialog::exec() {
 			skin = gmenu2x->confStr["skin"];
 		}
 
-		gmenu2x->setBackground(this->bg, "skins/" + skin + "/wallpapers/" + wallpapers[selected]);
+		//gmenu2x->setBackground(this->bg, "skins/" + skin + "/wallpapers/" + wallpapers[selected]);
 		gmenu2x->allyTTS(wallpapers[selected].c_str(), FAST_GAP_TTS, FAST_SPEED_TTS, 0);
 
-		drawDialog(gmenu2x->s);
+		drawDialog(gmenu2x->s, true, true, "skins/" + skin + "/wallpapers/" + wallpapers[selected]);
 
 		if (wallpaper.size() < 1) {
 			MessageBox mb(gmenu2x, gmenu2x->tr["No wallpapers available"]);
